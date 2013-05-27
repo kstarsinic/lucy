kstarsinic/lucy

commit c33d5cb3e9ed70a55214aa54c6760dc853206865

Support object types from other parcels without prefix
Resolve types after all classes have been parsed.

This means that caching of C representations has to be postponed. For
now, the C representations are cached after the types have been
resolved. This is error-prone and requires some hacks for methods. It
might be better to move to a scheme where the C strings are not cached
until they're used for the first time.

TODO: Add Perl bindings and fix Perl tests

clownfish/compiler/src/CFCMethod.c
@@ -105,12 +105,30 @@ CFCMethod_init(CFCMethod *self, CFCParcel *parcel, const char *exposure,
                      docucomment, false);
     FREEMEM(micro_sym);
 
+    self->macro_sym     = CFCUtil_strdup(macro_sym);
+    self->is_final      = is_final;
+    self->is_abstract   = is_abstract;
+
+    self->full_override_sym = NULL;
+
+    // Assume that this method is novel until we discover when applying
+    // inheritance that it overrides another.
+    self->is_novel = true;
+
+    return self;
+}
+
+void
+CFCMethod_resolve_types(CFCMethod *self, struct CFCClass **classes) {
+    CFCFunction_resolve_types((CFCFunction*)self, classes);
+
     // Verify that the first element in the arg list is a self.
-    CFCVariable **args = CFCParamList_get_variables(param_list);
+    CFCVariable **args = CFCParamList_get_variables(self->function.param_list);
     if (!args[0]) { CFCUtil_die("Missing 'self' argument"); }
     CFCType *type = CFCVariable_get_type(args[0]);
     const char *specifier = CFCType_get_specifier(type);
     const char *prefix    = CFCMethod_get_prefix(self);
+    const char *class_name = CFCMethod_get_class_name(self);
     const char *last_colon = strrchr(class_name, ':');
     const char *struct_sym = last_colon ? last_colon + 1 : class_name;
     char *wanted = CFCUtil_sprintf("%s%s", prefix, struct_sym);
@@ -121,19 +139,9 @@ CFCMethod_init(CFCMethod *self, CFCParcel *parcel, const char *exposure,
                     class_name, specifier);
     }
 
-    self->macro_sym     = CFCUtil_strdup(macro_sym);
-    self->is_final      = is_final;
-    self->is_abstract   = is_abstract;
-
     // Derive more symbols.
     const char *full_func_sym = CFCMethod_implementing_func_sym(self);
     self->full_override_sym = CFCUtil_sprintf("%s_OVERRIDE", full_func_sym);
-
-    // Assume that this method is novel until we discover when applying
-    // inheritance that it overrides another.
-    self->is_novel = true;
-
-    return self;
 }
 
 void
@@ -220,6 +228,9 @@ CFCMethod_finalize(CFCMethod *self) {
                         self->function.param_list,
                         self->function.docucomment, true,
                         self->is_abstract);
+    // Hack
+    CFCClass *dummy_class = NULL;
+    CFCMethod_resolve_types(finalized, &dummy_class);
     return finalized;
 }
 

clownfish/compiler/src/CFCTestType.c
@@ -16,11 +16,17 @@
 
 #define CFC_USE_TEST_MACROS
 #include "CFCBase.h"
+#include "CFCClass.h"
 #include "CFCParcel.h"
 #include "CFCParser.h"
 #include "CFCTest.h"
 #include "CFCType.h"
 #include "CFCUtil.h"
+
+#ifndef true
+  #define true 1
+  #define false 0
+#endif
 
 static void
 S_run_tests(CFCTest *test);
@@ -54,7 +60,7 @@ S_run_composite_tests(CFCTest *test);
 
 const CFCTestBatch CFCTEST_BATCH_TYPE = {
     "Clownfish::CFC::Model::Type",
-    353,
+    361,
     S_run_tests
 };
 
@@ -287,11 +293,17 @@ S_run_object_tests(CFCTest *test) {
         for (int i = 0; i < 4; ++i) {
             const char *specifier = specifiers[i];
 
+            char *class_code = CFCUtil_sprintf("class %s {}", specifier);
+            CFCClass *klass = CFCTest_parse_class(test, parser, class_code);
+            CFCClass *class_list[2] = { klass, NULL };
+            FREEMEM(class_code);
+
             static const char *prefixes[2] = { "", "neato_" };
             char *expect = CFCUtil_sprintf("neato_%s", specifier);
             for (int j = 0; j < 2; ++j) {
                 char *src = CFCUtil_sprintf("%s%s*", prefixes[j], specifier);
                 CFCType *type = CFCTest_parse_type(test, parser, src);
+                CFCType_resolve(type, class_list);
                 STR_EQ(test, CFCType_get_specifier(type), expect,
                        "object_type_specifier: %s", src);
                 OK(test, CFCType_is_object(type), "%s is_object", src);
@@ -312,16 +324,26 @@ S_run_object_tests(CFCTest *test) {
                 FREEMEM(src);
                 CFCBase_decref((CFCBase*)type);
             }
+
+            CFCBase_decref((CFCBase*)klass);
+            CFCClass_clear_registry();
         }
 
         CFCBase_decref((CFCBase*)neato_parcel);
         CFCBase_decref((CFCBase*)parser);
     }
 
+    CFCParcel *neato_parcel = CFCParcel_new("Neato", NULL, NULL);
+    CFCClass *foo_class
+        = CFCClass_create(neato_parcel, NULL, "Foo", NULL, NULL, NULL, NULL,
+                          NULL, false, false);
+    CFCClass *class_list[2] = { foo_class, NULL };
     CFCType *foo = CFCType_new_object(0, NULL, "Foo", 1);
+    CFCType_resolve(foo, class_list);
 
     {
         CFCType *another_foo = CFCType_new_object(0, NULL, "Foo", 1);
+        CFCType_resolve(another_foo, class_list);
         OK(test, CFCType_equals(foo, another_foo), "equals");
         CFCBase_decref((CFCBase*)another_foo);
     }
@@ -335,13 +357,18 @@ S_run_object_tests(CFCTest *test) {
 
     {
         CFCParcel *foreign_parcel = CFCParcel_new("Foreign", NULL, NULL);
-        CFCParcel_register(foreign_parcel);
+        CFCClass *foreign_foo_class
+            = CFCClass_create(foreign_parcel, NULL, "Foo", NULL, NULL, NULL,
+                              NULL, NULL, false, false);
+        CFCClass *foreign_class_list[2] = { foreign_foo_class, NULL };
         CFCType *foreign_foo = CFCType_new_object(0, foreign_parcel, "Foo", 1);
+        CFCType_resolve(foreign_foo, foreign_class_list);
         OK(test, !CFCType_equals(foo, foreign_foo),
            "different parcel spoils equals");
         STR_EQ(test, CFCType_get_specifier(foreign_foo), "foreign_Foo",
                "prepend parcel prefix to specifier");
         CFCBase_decref((CFCBase*)foreign_parcel);
+        CFCBase_decref((CFCBase*)foreign_foo_class);
         CFCBase_decref((CFCBase*)foreign_foo);
     }
 
@@ -366,8 +393,10 @@ S_run_object_tests(CFCTest *test) {
         CFCBase_decref((CFCBase*)string_type);
     }
 
+    CFCBase_decref((CFCBase*)foo_class);
     CFCBase_decref((CFCBase*)foo);
 
+    CFCClass_clear_registry();
     CFCParcel_reap_singletons();
 }
 
@@ -485,7 +514,9 @@ S_run_composite_tests(CFCTest *test) {
     }
 
     {
+        CFCClass *class_list[1] = { NULL };
         CFCType *foo_array = CFCTest_parse_type(test, parser, "foo_t[]");
+        CFCType_resolve(foo_array, class_list);
         STR_EQ(test, CFCType_get_array(foo_array), "[]", "get_array");
         STR_EQ(test, CFCType_to_c(foo_array), "foo_t",
                "array subscripts not included by to_c");

clownfish/compiler/src/CFCClass.c
@@ -459,6 +459,22 @@ CFCClass_fresh_method(CFCClass *self, const char *sym) {
     return NULL;
 }
 
+void
+CFCClass_resolve_types(CFCClass *self, CFCClass **classes) {
+    for (size_t i = 0; self->functions[i] != NULL; i++) {
+        CFCFunction_resolve_types(self->functions[i], classes);
+    }
+    for (size_t i = 0; self->methods[i] != NULL; i++) {
+        CFCMethod_resolve_types(self->methods[i], classes);
+    }
+    for (size_t i = 0; self->member_vars[i] != NULL; i++) {
+        CFCVariable_resolve_type(self->member_vars[i], classes);
+    }
+    for (size_t i = 0; self->inert_vars[i] != NULL; i++) {
+        CFCVariable_resolve_type(self->inert_vars[i], classes);
+    }
+}
+
 // Pass down member vars to from parent to children.
 static void
 S_bequeath_member_vars(CFCClass *self) {

clownfish/compiler/src/CFCClass.h
@@ -150,6 +150,11 @@ CFCClass_fresh_method(CFCClass *self, const char *sym);
 struct CFCMethod*
 CFCClass_find_novel_method(CFCClass *self, const char *sym);
 
+/** Find the actual class of all object variables without prefix.
+ */
+void
+CFCClass_resolve_types(CFCClass *self, CFCClass **classes);
+
 /** Bequeath all inherited methods and members to children.
  */
 void

clownfish/compiler/src/CFCDumpable.c
@@ -110,14 +110,15 @@ CFCDumpable_add_dumpables(CFCDumpable *self, CFCClass *klass) {
 
 static CFCMethod*
 S_make_method_obj(CFCClass *klass, const char *method_name) {
-    const char *klass_struct_sym = CFCClass_get_struct_sym(klass);
+    const char *klass_full_struct_sym = CFCClass_full_struct_sym(klass);
     const char *klass_name   = CFCClass_get_class_name(klass);
     const char *klass_cnick  = CFCClass_get_cnick(klass);
     CFCParcel  *klass_parcel = CFCClass_get_parcel(klass);
 
     CFCType *return_type
-        = CFCType_new_object(CFCTYPE_INCREMENTED, klass_parcel, "Obj", 1);
-    CFCType *self_type = CFCType_new_object(0, klass_parcel, klass_struct_sym, 1);
+        = CFCType_new_object(CFCTYPE_INCREMENTED, klass_parcel, "lucy_Obj", 1);
+    CFCType *self_type = CFCType_new_object(0, klass_parcel,
+                                            klass_full_struct_sym, 1);
     CFCVariable *self_var = CFCVariable_new(NULL, NULL, NULL, NULL, "self",
                                             self_type, false);
     CFCParamList *param_list = NULL;
@@ -127,7 +128,8 @@ S_make_method_obj(CFCClass *klass, const char *method_name) {
         CFCParamList_add_param(param_list, self_var, NULL);
     }
     else if (strcmp(method_name, "Load") == 0) {
-        CFCType *dump_type = CFCType_new_object(0, klass_parcel, "Obj", 1);
+        CFCType *dump_type = CFCType_new_object(0, klass_parcel, "lucy_Obj",
+                                                1);
         CFCVariable *dump_var = CFCVariable_new(NULL, NULL, NULL, NULL, "dump",
                                                 dump_type, false);
         param_list = CFCParamList_new(false);
@@ -143,6 +145,9 @@ S_make_method_obj(CFCClass *klass, const char *method_name) {
     CFCMethod *method = CFCMethod_new(klass_parcel, "public", klass_name,
                                       klass_cnick, method_name, return_type,
                                       param_list, NULL, false, false);
+    // Hack
+    CFCClass *dummy_class = NULL;
+    CFCMethod_resolve_types(method, &dummy_class);
 
     CFCBase_decref((CFCBase*)param_list);
     CFCBase_decref((CFCBase*)self_type);

clownfish/compiler/src/CFCFunction.c
@@ -84,6 +84,12 @@ CFCFunction_init(CFCFunction *self, CFCParcel *parcel, const char *exposure,
 }
 
 void
+CFCFunction_resolve_types(CFCFunction *self, struct CFCClass **classes) {
+    CFCType_resolve(self->return_type, classes);
+    CFCParamList_resolve_types(self->param_list, classes);
+}
+
+void
 CFCFunction_destroy(CFCFunction *self) {
     CFCBase_decref((CFCBase*)self->return_type);
     CFCBase_decref((CFCBase*)self->param_list);

clownfish/compiler/src/CFCFunction.h
@@ -29,6 +29,7 @@ struct CFCParcel;
 struct CFCType;
 struct CFCDocuComment;
 struct CFCParamList;
+struct CFCClass;
 
 #ifdef CFC_NEED_FUNCTION_STRUCT_DEF
 #define CFC_NEED_SYMBOL_STRUCT_DEF
@@ -109,6 +110,11 @@ CFCFunction_micro_sym(CFCFunction *self);
 
 int
 CFCFunction_public(CFCFunction *self);
+
+/** Find the actual class of all object variables without prefix.
+ */
+void
+CFCFunction_resolve_types(CFCFunction *self, struct CFCClass **classes);
 
 #ifdef __cplusplus
 }

clownfish/compiler/src/CFCHierarchy.c
@@ -197,6 +197,9 @@ CFCHierarchy_build(CFCHierarchy *self) {
     for (size_t i = 0; self->includes[i] != NULL; i++) {
         S_parse_cf_files(self, self->includes[i], 1);
     }
+    for (int i = 0; self->classes[i] != NULL; i++) {
+        CFCClass_resolve_types(self->classes[i], self->classes);
+    }
     S_connect_classes(self);
     for (size_t i = 0; self->trees[i] != NULL; i++) {
         CFCClass_grow_tree(self->trees[i]);

commit cb1f2cf5ba0747b761430d6328972313c46b1b5f

Fix memory leaks introduced by commit c33d5cb
c33d5cb Support object types from other parcels without prefix

clownfish/compiler/src/CFCMethod.c
@@ -228,9 +228,6 @@ CFCMethod_finalize(CFCMethod *self) {
                         self->function.param_list,
                         self->function.docucomment, true,
                         self->is_abstract);
-    // Hack
-    CFCClass *dummy_class = NULL;
-    CFCMethod_resolve_types(finalized, &dummy_class);
     return finalized;
 }
 

clownfish/compiler/src/CFCTestType.c
@@ -394,6 +394,7 @@ S_run_object_tests(CFCTest *test) {
         CFCBase_decref((CFCBase*)string_type);
     }
 
+    CFCBase_decref((CFCBase*)neato_parcel);
     CFCBase_decref((CFCBase*)foo_class);
     CFCBase_decref((CFCBase*)foo);
 